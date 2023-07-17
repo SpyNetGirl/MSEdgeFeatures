f4d6da8510dcc5e0839e4e96955608fc7f336bf6
Violet <spynetgirl@outlook.com>
2023-07-17 22:55:30 +0100

    Updated the main script
    Instead of creating Edge shortcut .lnk a .bat file will be created.
    it will allow for much longer list of features to be used with Edge canary and no longer limited by the File Explorer's limit of Target field.

--- Update-old method-using Edge shortcut.ps1
+++ Update-old method-using Edge shortcut.ps1
@@ -175,26 +175,49 @@ $($added | ForEach-Object {"* $_`n"})
 
     New-Item -Path ".\EdgeCanaryShortcutMaker.ps1" -Force
 
+
     $AddedArray = $Added -join ","
     $PreArguments = "--enable-features=$AddedArray"
     $PreArguments = $PreArguments.TrimEnd(",")
-    # Content to add the PowerShell script that creates the bat script that launches Edge with new features
-    $ContentToAdd = @"
-
-    `$VersionToUse = `"$Version`"
+    
+    $contenttoadd = @"
     
     `$arguments = `"$PreArguments`"
     
-`$content = @`"
-powershell.exe -WindowStyle hidden -Command "& \"C:\Users\`$env:USERNAME\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\" --enable-features=`$arguments"
-`"@
-
-    `$content | Out-File -FilePath "C:\Users\`$env:USERNAME\Downloads\EDGECAN Launcher `$VersionToUse.bat"
+    function New-Shortcut {
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory = `$true)][ValidateNotNullOrEmpty()][string]`$Path,
+            [Parameter(Mandatory = `$true)][ValidateNotNullOrEmpty()][string]`$Target,
+            [Parameter(Mandatory = `$true)][ValidateNotNullOrEmpty()][string]`$Description,
+            [Parameter(Mandatory = `$true)][ValidateNotNullOrEmpty()][string]`$WorkingDirectory,
+            [string]`$Arguments,
+            [string]`$Icon
+        )
+        if (!(Test-Path `$Path)) {
+            if (!(Test-Path `$Path)) {
+                `$WshShell = New-Object -ComObject WScript.Shell
+                `$Shortcut = `$WshShell.CreateShortcut(`$Path)
+                `$Shortcut.TargetPath = `$Target
+                if (![string]::IsNullOrEmpty(`$Arguments)) { `$Shortcut.Arguments = `$Arguments }
+                if (![string]::IsNullOrEmpty(`$Icon)) { `$Shortcut.IconLocation = `$Icon }
+                `$Shortcut.Description = `$Description
+                `$Shortcut.WorkingDirectory = `$WorkingDirectory
+                `$Shortcut.Save()
+            }
+        }
+    }
+    if (Test-Path "C:\Users\`$env:USERNAME\Downloads\EDGECAN.lnk") { Remove-Item "C:\Users\`$env:USERNAME\Downloads\EDGECAN.lnk" }
+    New-Shortcut -Path "C:\Users\`$env:USERNAME\Downloads\EDGECAN.lnk" ``
+        -Target "C:\Users\`$env:USERNAME\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe" ``
+        -Description "Microsoft Edge" ``
+        -WorkingDirectory "C:\Users\`$env:USERNAME\AppData\Local\Microsoft\Edge SxS\Application" ``
+        -Icon "C:\Users\`$env:USERNAME\AppData\Local\Microsoft\Edge SxS\User Data\Default\Edge Profile.ico" ``
+        -Arguments `$arguments
     
 "@
     
-    Set-Content -Value $ContentToAdd -Path ".\EdgeCanaryShortcutMaker.ps1" -Force
- 
+    Set-Content -Value $contenttoadd -Path ".\EdgeCanaryShortcutMaker.ps1" -Force
 
     #endregion Edge-Canary-ShortCut-Maker-Code
 
@@ -207,11 +230,11 @@ powershell.exe -WindowStyle hidden -Command "& \"C:\Users\`$env:USERNAME\AppData
     # Without this, the email would have empty content for the body of the Release
 
     $GitHubReleaseBodyContent = @"
-    # <img width="35" src="https://github.com/HotCakeX/Harden-Windows-Security/raw/main/images/WebP/Edge%20Canary.webp"> Automated update
+# <img width="35" src="https://github.com/HotCakeX/Harden-Windows-Security/raw/main/images/WebP/Edge%20Canary.webp"> Automated update
         
-    ## Processed at: $(Get-Date -AsUTC) (UTC+00:00)`n
+## Processed at: $(Get-Date -AsUTC) (UTC+00:00)`n
 
-    Visit the GitHub's release section for full details on how to use it:
+Visit the GitHub's release section for full details on how to use it:
 https://github.com/HotCakeX/MSEdgeFeatures/releases/tag/$Version
     
 ### $($added.count) New features were added
