73c59b6796f2101e4b8a6041f6e0e002eccfb62d
HotCakeX <spynetgirl@outlook.com>
2025-08-07 19:47:42 +0000

    Automated Update

---
+++ EdgeCanaryShortcutMaker.ps1
@@ -0,0 +1,10 @@
+
+$FullVersionToUse = "140.0.3483.0"
+
+$Arguments = "--enable-features=msTranslateVideoPerLanguageSTModel"
+
+$content = @"
+powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
+"@
+
+$content | Out-File -FilePath "C:\Users\$env:USERNAME\Downloads\EDGECAN Launcher $FullVersionToUse.bat"
