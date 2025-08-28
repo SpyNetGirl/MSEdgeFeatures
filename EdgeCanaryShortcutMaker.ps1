dcbdd8d0520bacd437d96128cceca1c800efd5f3
HotCakeX <spynetgirl@outlook.com>
2025-08-28 16:26:38 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "141.0.3520.0"
+$FullVersionToUse = "141.0.3522.0"
 
-$Arguments = "--enable-features=msEdgeAutofillServerVersion,msEdgeAutofilledValueAcceptedHandlerAutofillCore,msEdgeAutofilledValueAcceptedHandlerPassword,msEdgeBackgroundRestartManagerDisableIfExtensionsLoaded,msEdgeUCNtpCardsIgnoreMinCards,msTechScamSensorNetwork"
+$Arguments = "--enable-features=msEdgeShoppingDisableDefaultExperience,msHistoryActiveVisitDuration,msTechScamSensorNetworkToggleOnByDefault,msUndersideChatTopChrome"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
