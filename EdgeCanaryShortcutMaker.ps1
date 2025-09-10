b60ec3b63e9cf428773621200969d854bbfb65f9
HotCakeX <spynetgirl@outlook.com>
2025-09-10 01:26:17 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "142.0.3546.0"
+$FullVersionToUse = "142.0.3547.0"
 
-$Arguments = "--enable-features=msEdgeImmersiveSoftFREWithRuby"
+$Arguments = "--enable-features=msEdgeSettingsSearchWeightRanking"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
