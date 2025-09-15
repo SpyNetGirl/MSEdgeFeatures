a9e85b56dc57b7c60e094c361c5f8909db44ceaa
HotCakeX <spynetgirl@outlook.com>
2025-09-15 16:26:31 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "142.0.3553.0"
+$FullVersionToUse = "142.0.3558.0"
 
-$Arguments = "--enable-features=msOnDeviceMultiModal,msTieUnpackedExtensionsToDevMode,msUnpublishedExtensionsConsumerUX"
+$Arguments = "--enable-features=msDesktopModeVisualSearch,msEdgeSettingsEnterpriseEdgePreview,msEdgeShoppingCopilotFlyoutSelfhostExperience,msEdgeShoppingSkipArbitratorForCopilotFlyoutForTesting,msImproveDefaultBrowser2ClickUxExperience,msShoppingUapiExp26,msShoppingUapiExp26Failure,msShoppingUapiExp26Success,msShoppingUapiExp27,msShoppingUapiExp27Failure,msShoppingUapiExp27Success,msShoppingUapiExp29,msShoppingUapiExp29Failure,msShoppingUapiExp29Success,msShoppingUapiExp44,msShoppingUapiExp44Failure,msShoppingUapiExp44Success,msShoppingUapiExp48,msShoppingUapiExp48Failure,msShoppingUapiExp48Success,msShoppingUapiExp49,msShoppingUapiExp49Failure,msShoppingUapiExp49Success"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
