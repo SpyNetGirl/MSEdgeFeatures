0f4b64dd28334a534be1e988891aa8ea93cefcf1
HotCakeX <spynetgirl@outlook.com>
2025-10-06 16:26:50 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "142.0.3595.0"
+$FullVersionToUse = "143.0.3601.0"
 
-$Arguments = "--enable-features=msCommercialCopilotChatWebUI2"
+$Arguments = "--enable-features=msClassifyComposerActionChat,msCloudProfileReporting,msCloudProfileReportingIncludeAllData,msEdgeComposerShowCocoMode,msEdgeComposerShowStudyMode,msEdgeToolsBlockNavigationUsingBloomFilter,msEdgeToolsScreenshotEncoding,msEdgeToolsScreenshotHeight,msEdgeToolsScreenshotMaxPayloadSize,msEdgeToolsScreenshotWidth,msInitSendLockedStateNotify,msSegmentationPlatformUrlInterestsInterceptOptimizationFeature,msSummarizeWithCopilotNewBadge,msTokenizationAutofillInlineEnabled,msWorkspaceMigrationTrigger"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
