8d146fd9b9806ae337f893aba4a71c8de4d9785a
HotCakeX <spynetgirl@outlook.com>
2025-10-16 01:29:29 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "143.0.3620.0"
+$FullVersionToUse = "143.0.3621.0"
 
-$Arguments = "--enable-features=MscopilotSettings,msEdgePinpointJustInTimeDocAI,msEdgePinpointJustInTimeEduAI,msEdgePinpointJustInTimeEmailAI,msSendToCopilotInContextMenu,msuYE1"
+$Arguments = "--enable-features=msPeriodPressurePreReadFileTrigger,msSegmentationPlatformUrlInterestsCopilotModeUpsellFeature,msSkipSWUpdateOnDevtoolsDetachIfModified,msWhatsNewContentVisible,msWhatsNewContentVisibleChromePB50,msWhatsNewContentVisibleChromePB60,msWhatsNewContentVisibleChromePB70,msWhatsNewContentVisibleChromePB80,msWhatsNewContentVisibleChromePB90,msWhatsNewContentVisibleCopilotModeEligible,msWhatsNewContentVisibleEdgePB,msWhatsNewContentVisibleNonEdgePB,msWhatsNewContentVisibleNotNtpFeedEngaged,msWhatsNewContentVisibleNtpFeedEngaged,msWhatsNewContentVisibleSourceChromePBOptimizations,msWhatsNewContentVisibleSourceEdgePBOptimizations,msWhatsNewContentVisibleSourceNonEdgePBOptimizations,msWhatsNewContentVisibleSourceOptimizations,msWhatsNewContentVisibleUnknownNtpFeedEngaged,msWhatsNewUpdateCompleteVisible,msWhatsNewUpdateCompleteVisibleFromHidden,msWhatsNewUpdateCompleteVisibleOnLaunch"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
