7aa11ecf3cb4d63c611eee28700c84bdaf25987c
HotCakeX <spynetgirl@outlook.com>
2025-12-03 16:33:00 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "144.0.3713.0"
+$FullVersionToUse = "144.0.3715.0"
 
-$Arguments = "--enable-features=msEnableNextSiteSuggestionInCodeForIndia"
+$Arguments = "--enable-features=msComposerChatSuggestions,msEdgeJourneysDisableSummaryCooldown,msSegmentationPlatformWhatsNewPageEligibilityOverrideForTesting,msSegmentationPlatformWhatsNewPageReminderFeature,msSegmentationPlatformWhatsNewPageReminderLaunchNotification,msWhatsNewPageReminderAllowedLocales,msWhatsNewPageReminderAnyBadgeShownDurationInDays,msWhatsNewPageReminderBrowserUsageFetchDurationInDays,msWhatsNewPageReminderBrowserVersionToCheck,msWhatsNewPageReminderCheckByAlreadySeen,msWhatsNewPageReminderControlVersion,msWhatsNewPageReminderExecutionCadenceSeconds,msWhatsNewPageReminderFreshnessInMinutes,msWhatsNewPageReminderInitialExecutionDelaySeconds,msWhatsNewPageReminderMinChromeUsageMB,msWhatsNewPageReminderMinChromeUsagePercentage,msWhatsNewPageReminderMinDaysSinceFre,msWhatsNewPageReminderModelVersion,msWhatsNewPageReminderNotificationExpireDurationInDays"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
