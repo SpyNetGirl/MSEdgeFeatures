fa48d7eaf259a1d1c669408365babf3339b89c0e
HotCakeX <spynetgirl@outlook.com>
2025-12-20 01:43:37 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "145.0.3746.0"
+$FullVersionToUse = "145.0.3748.0"
 
-$Arguments = "--enable-features=msStudioNtpEnableContentMarketOverride,msStudioNtpForceCookieBanner,msWhatsNewPageReminderCheckIsEdgePinnedToTaskbar"
+$Arguments = "--enable-features=MsHTMLMediaElementEncryptedMedia,msCopilotModeUpsellEdgePBUrlInterestGroupNtpOrDhpNavigation,msCopilotModeUpsellNonEdgePBUrlInterestGroupNtpOrDhpNavigation,msCopilotModeUpsellUrlInterestGroupNtpOrDhpNavigation,msEdgeShoppingPcNotificationOption1,msEdgeShoppingPcNotificationOption2,msHistorySuggestFrecencyFormulaV21,msHistorySuggestFrecencyFormulaV22,msHistorySuggestFrecencyFormulaV23,msHistorySuggestFrecencyFormulaV2AgeLimit,msHistorySuggestFrecencyFormulaV2Threshold,msHistorySuggestFrecencyFormulaV2UniqueDays,msNurturingEdgeCopilotJitEmailCompose"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
