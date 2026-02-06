d50de7daa8e4b5c43e18aa65742f582a059634a9
HotCakeX <spynetgirl@outlook.com>
2026-02-06 16:50:11 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "146.0.3843.0"
+$FullVersionToUse = "146.0.3844.0"
 
-$Arguments = "--enable-features=msCommercialToolsBypassCopilotMode,msComponentUpdaterBridge3pSerpTrigger,msComponentUpdaterBridgeArbitrationTrigger,msComponentUpdaterBridgeEntityExtractionTrigger,msComponentUpdaterBridgeSidebarTrigger,msComponentUpdaterBridgeSignalTriggersTrigger,msEdgeNtpSkipUaOverrideInheritance,msEnhanceEdgeNTPhijackingMitigation,msFeatureChromeBrowserUsageGreaterThanThresholdTrigger,msFeatureChromePBTrigger,msFeatureEdgeBrowserUsageLessThanThresholrTrigger,msFeatureFireTriggersForChromePBUser,msFeatureNonMVUUserTrigger,msFeatureTotalBrowserUsageGreaterThanThresholdTrigger,msPCMHijackingPatchNTPFuncDetected,msPetrolMsalAuthentication,msSegmentationPlatformUrlInterestsJustInTimeCopilotCapability"
+$Arguments = "--enable-features=msAutofillEdgeCouponsAutoApply,msBingReplaceLanguageURLQueryParameter,msComposerOuterframeTextBuffer,msComposerOuterframeTextBufferVisibility,msDesktopModeExplicitOptin,msEdgeMediaControlsShowThumbnail,msEnabledProviders,msM365LinkInsightsForOutlookTrigger"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
