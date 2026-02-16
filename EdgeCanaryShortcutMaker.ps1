f09ddcc4b4eecb26b555fbd3f6f965878ab26019
HotCakeX <spynetgirl@outlook.com>
2026-02-16 16:49:16 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "147.0.3859.0"
+$FullVersionToUse = "147.0.3864.0"
 
-$Arguments = "--enable-features=msM365LinkInsightsCheckOutlookAutoOpenSwitch,msWinPdfOCR"
+$Arguments = "--enable-features=msAddExtraNTPTabWithLaunch,msAutolaunchEEA,msAutolaunchEEAComplianceActivationFlowPreempted,msAutolaunchEEAComplianceAsterHighUsageDMA,msAutolaunchEEAComplianceCIActiveDMA,msEdgeCopilotModeEnabledDefaultAAD,msEdgeNotDefaultBrowser,msLlmConsumerDlpRedirectToCopilot,msNurturingAutolaunchForEEA,msNurturingGroupEdgeMobileUpsellSynchronousRewards,msUserColorThemeDetectedOnThemesImport"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
