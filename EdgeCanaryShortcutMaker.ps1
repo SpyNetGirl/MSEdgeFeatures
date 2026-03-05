0e243777d1249e636fcae34ca03e769f82c00303
HotCakeX <spynetgirl@outlook.com>
2026-03-05 02:14:14 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "147.0.3896.0"
+$FullVersionToUse = "147.0.3897.0"
 
-$Arguments = "--enable-features=MSPAuth,msAutolaunchActivationUpsell,msEdgeShoppingCombinedShoppingExperience,msEdgeToolsPageScreenshot,msNurturingAutolaunchInfobarAsterUsageQualifiedTrigger,msNurturingAutolaunchOnLaunchInfobarTrigger,msNurturingAutolaunchOnLaunchOptInVariant,msNurturingAutolaunchOnLaunchOptOutVariant,msOnDeviceSpeechRecognitionAPI"
+$Arguments = "--enable-features=MSANotSignedInLinkId,MSASignedInLinkId,MSCopilot,msEdgeIncludeExtendedExtensionDiagnosticsInFeedback,msM365LinkInsightsDebounceDelaySeconds,msM365LinkInsightsMinimumPageContentLength,msMdaPriorityOverMam,msSensitiveDomains,msWhatsNewContentVisibleRewardsHVAEligible,msWhatsNewContentVisibleRewardsHVAMSANotSignedIn,msWhatsNewContentVisibleRewardsHVAMSASignedIn,msWhatsNewRewardsHVAEligible,msWhatsNewRewardsHVAMSANotSignedIn,msWhatsNewRewardsHVAMSASignedIn,msWhatsNewUpdateCompleteVisibleRewardsHVAEligible,msWhatsNewUpdateCompleteVisibleRewardsHVAMSANotSignedIn,msWhatsNewUpdateCompleteVisibleRewardsHVAMSASignedIn"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
