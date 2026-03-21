a49d55d0fbd8d9ba78c304ec89693fb9931d1880
HotCakeX <spynetgirl@outlook.com>
2026-03-21 02:08:13 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "148.0.3928.0"
+$FullVersionToUse = "148.0.3929.0"
 
-$Arguments = "--enable-features=msCopilotInStandardSidePanel,msPinnedSitesStaleTileRemoval,msPinnedSitesStaleTileReplacement,msSegmentationPlatformTileStalenessFeature,msStaleTileRemoveTAM30DaysTrigger,msStaleTileRemoveTAM45DaysTrigger,msStaleTileReplaceRegionalSuggestionsTrigger,msStaleTileReplaceTAM30DaysTrigger,msStaleTileReplaceTAM45DaysTrigger"
+$Arguments = "--enable-features=msAutofillEdgeInlineDeleteSuggestion,msEdgeToolsWebMcp,msPdfLazyLoadBookmarkActions"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
