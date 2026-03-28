48db4979ded57e76c465477afe53cd78514b1d10
HotCakeX <spynetgirl@outlook.com>
2026-03-28 08:43:24 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "148.0.3942.0"
+$FullVersionToUse = "148.0.3943.0"
 
-$Arguments = "--enable-features=msEdgeCloudPolicyApplySourceV2,msEdgeCloudPolicyV1,msEdgeCopilotNtpDailyRefreshBackgroundAAD,msNurturingFrequentSitesPinUpsell"
+$Arguments = "--enable-features=msCopilotPerTab,msEdgeImportPreventDisallowedExtensionsInstall,msEdgeReaderModeReadAloudClicked,msUndersideV2ComponentExtension"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
