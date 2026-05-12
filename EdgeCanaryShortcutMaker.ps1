7db596d8c6bc612f7136c82a08ef140c2aaf7ed1
HotCakeX <spynetgirl@outlook.com>
2026-05-12 18:01:48 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "149.0.4021.0"
+$FullVersionToUse = "150.0.4030.0"
 
-$Arguments = "--enable-features=msDesktopModeImageDropToSearch,msEdgePostScoobeChromePBTrigger,msEdgePostScoobeEdgePBTrigger,msEdgePostScoobeUnknownOtherPBTrigger,msEdgeWindowsWelcomeUpdateChromePrimaryBrowserTrigger,msEdgeWindowsWelcomeUpdateEdgePrimaryBrowserTrigger,msEdgeWindowsWelcomeUpdatePageVisitedTrigger,msEdgeWindowsWelcomeUpdateUnknownOrOtherPrimaryBrowserTrigger"
+$Arguments = "--enable-features=MsComposeInlineBlueDotParams,msAsterAppBoundDataSelectedForImport,msEdgeBrowserHasslesUntriggeredSubpage,msEdgeJourneysOnDeviceSummary,msEdgeToolsAutoLockRemoteTabs,msNurturingContextQueryOptimization,msNurturingDialogUpsell"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
