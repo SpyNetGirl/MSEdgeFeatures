f2aaaacb25191e0bc2fc739e2a8f26a9616f3b14
HotCakeX <spynetgirl@outlook.com>
2026-06-09 18:08:33 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "151.0.4083.0"
+$FullVersionToUse = "151.0.4084.0"
 
-$Arguments = "--enable-features=msEnableMaiDesignSystemForMica,msEntertainmentSpotifyClientDetected,msEntertainmentSpotifyDetected,msEntertainmentSpotifyWebDetected"
+$Arguments = "--enable-features=msEdgeRoseCooldownDays,msEdgeRoseLimitHit"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
