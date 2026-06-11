cb96cc9cec83599b0c920e4ad08ce95566ef818f
HotCakeX <spynetgirl@outlook.com>
2026-06-11 18:48:36 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "151.0.4087.0"
+$FullVersionToUse = "151.0.4088.0"
 
-$Arguments = "--enable-features=msOneJSDhpStartNavigation,msOneJSNonPrerenderDhpStartNavigation,msOneJSNonPrerenderNtpNavigation,msOneJSNonPrerenderNtpOrDhpStartNavigation,msOneJSNonPrerenderNtpOrPostFreDhpNavigation,msOneJSNonPrerenderNtpStartNavigation,msOneJSNonPrerenderPostFreDhpNavigation,msOneJSNtpNavigation,msOneJSNtpOrDhpStartNavigation,msOneJSNtpOrPostFreDhpNavigation,msOneJSNtpStartNavigation,msOneJSPostFreDhpNavigation,msOneJSPrerenderDhpStartNavigation,msOneJSPrerenderNtpNavigation,msOneJSPrerenderNtpOrDhpStartNavigation,msOneJSPrerenderNtpOrPostFreDhpNavigation,msOneJSPrerenderNtpStartNavigation,msOneJSPrerenderPostFreDhpNavigation"
+$Arguments = "--enable-features=msDisableGwpAsanOnLowMemoryDevices,msNurturingDefaultPDFInfobar,msNurturingPdfInfobarChromeUsage50Trigger,msNurturingPdfInfobarChromeUsage90Trigger,msNurturingPdfInfobarEligibleTrigger,msNurturingPdfInfobarTrigger"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
