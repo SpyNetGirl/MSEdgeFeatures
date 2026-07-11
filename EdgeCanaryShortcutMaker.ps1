cbbcd45f91fce16e5b2e21805a045b3740d31b19
HotCakeX <spynetgirl@outlook.com>
2026-07-11 02:45:38 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "152.0.4147.0"
+$FullVersionToUse = "152.0.4148.0"
 
-$Arguments = "--enable-features=msEdgeSpaceworksExternalLinkAutoRouting"
+$Arguments = "--enable-features=msoridCopilotDeviceFileUploadEnabled,msoridCopilotDeviceReadEnabled,msoridCopilotDeviceSearchEnabled,msoridCopilotDeviceWriteEnabled"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
