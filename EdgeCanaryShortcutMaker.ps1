bbcb63c54cfeae19d5e3eaf6f8fb569313b54200
HotCakeX <spynetgirl@outlook.com>
2026-08-19 16:34:27 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "153.0.4227.0"
+$FullVersionToUse = "153.0.4228.0"
 
-$Arguments = "--enable-features=msDelayLoadSandboxPolicy,msEdgeAutofillComplexFormDetection,msSkipSnapDelayLoadsInBrowserProcess,msSkipSnapDelayLoadsInRenderProcess,msSnapDelayLoads"
+$Arguments = "--enable-features=msEnableCrHostAuthPrivate"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
