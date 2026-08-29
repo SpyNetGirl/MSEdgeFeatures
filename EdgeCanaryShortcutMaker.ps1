de249111ad05048bf6fdd6daa354d65d0db49006
HotCakeX <spynetgirl@outlook.com>
2026-08-29 00:28:33 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "154.0.4242.0"
+$FullVersionToUse = "154.0.4245.0"
 
-$Arguments = "--enable-features="
+$Arguments = "--enable-features=msCardRoamingMSAv2,msEdgeReadingModeExtractionImprovements,msEdgeSkipPreReadMainDllWhileWindowless,msEdgeSuppressPreReadMainDllWhenNoActiveBrowser,msMsnNtpRendererFamilyOverrideForTesting,msOmniboxQFAcceptedAtLeastOncePerDay,msOmniboxQFAcceptedAtLeastTwicePerDay,msOmniboxZIAcceptedAtLeastOncePerDay,msOmniboxZIAcceptedAtLeastTwicePerDay,msSegmentationPlatformDefaultsNotificationTriggeringModelUseCU"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
