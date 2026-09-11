70f709e781bec7c088840667070e3193ead02937
HotCakeX <spynetgirl@outlook.com>
2026-09-11 19:02:36 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "155.0.4269.0"
+$FullVersionToUse = "155.0.4272.0"
 
-$Arguments = "--enable-features=msFeatureMmxUpsellFavoriteSave,msFeatureMmxUpsellOwnPasswordSave,msFeatureMmxUpsellSuggestedStrongPasswordSave,msNurturingGroupMobileGrowthFavoriteSave,msNurturingGroupMobileGrowthPasswordSave"
+$Arguments = "--enable-features=msAutopilotComputerUse,msCopilotProtectedSession,msD2MSettingsEntry,msEdgeCopilotBridgeWorkerPriority,msEdgeIntentsAddressBarChipAfterFlyout,msEdgeIntentsCopilotFullPage,msEdgeOmniboxPinToTaskbar,msEdgeOptimizationGuideManifestBroker,msEdgeProfileIDClearStaleMUIDOnInitialGeneration,msEdgeSearchHistoryNavAwareAggressiveWindow,msEdgeToolsSuppressStopActionOnTabClose,msEmxAdaptiveRefreshInterval,msNurturingSplitViewPinnable,msNurturingSplitViewTabSwitchTutorial,msNurturingUndefinedFeaure,msSwapInOnResumeForAnyPriority"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
