4d81be73903ef85fd9ca168f4e52c3284becd222
HotCakeX <spynetgirl@outlook.com>
2026-09-22 03:46:41 +0000

    Automated Update

--- EdgeCanaryShortcutMaker.ps1
+++ EdgeCanaryShortcutMaker.ps1
@@ -1,7 +1,7 @@
 
-$FullVersionToUse = "156.0.4292.0"
+$FullVersionToUse = "156.0.4293.0"
 
-$Arguments = "--enable-features=msAIModePrivateApis,msHideFavoritesStarOnNTP,msNurturingMSStartYJCompeteTextVolumn,msNurturingTaskbarLiveEventBadgesOptOut,msNurturingUndefinedFeaure,msPdfTranslateFromOmniboxBubble,msTaskbarLiveEventBadgesSettings,msWebUILiveEvents,msWhatsNewNurturingBackgroundTabOpened,msWhatsNewNurturingTreatmentClicked,msWhatsNewNurturingTreatmentShown"
+$Arguments = "--enable-features=msEdgeComposeOnDeviceRewrite,msEdgeComposeOnDeviceRewriteForAllQueries"
 
 $content = @"
 powershell.exe -WindowStyle hidden -Command "`$UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value;`$UserName = (Get-LocalUser | where-object -FilterScript {`$_.SID -eq `$UserSID}).name;Get-Process | where-object -FilterScript {`$_.path -eq \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`"} | ForEach-Object -Process {Stop-Process -Id `$_.id -Force -ErrorAction SilentlyContinue};& \`"C:\Users\`$UserName\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe\`" $Arguments"
